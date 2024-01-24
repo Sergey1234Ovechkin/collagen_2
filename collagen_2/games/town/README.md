ec58e2adf3faa0db3746afcc66a8b29a39d1a332
Sergey1234Ovechkin <145683225+Sergey1234Ovechkin@users.noreply.github.com>
2024-01-24 09:33:51 +0300

    Add files via upload

---
+++ collagen_2/games/town/README.md
@@ -0,0 +1,84 @@
+
+
+Для работы игры требуются модули forever(глобальная инсталяция) и socket.js 
+Установить модули на node.js командами:
+ npm install forever -g
+ npm install socket.js
+ 
+Основной скрипт игры - game_script.js
+
+Запуск игры -  forever start server.js
+
+
+
+Для того чтобы вставить спрайты своих персонаже в игру - открыть файл public/game_script.js
+
+///тип спрайта персонажа (название)
+var typeP1 = "men_wolf"; /// изменить названия персонажей на свои названия
+var typeP2 = "women_1";
+var typeP3 = "new_personage"; //можно добавить нового персонажа
+
+  ///в конце скрипта добавить в функцию код для выбора нового персонажа
+ ///форма выбора типа персонажа
+ StateMap.chose_personage = { 	
+		container: "chose_personage",
+		props: [["personage_1", "mousedown", "[name='personage_1']"], ["personage_2", "mousedown", "[name='personage_2']"],				 
+		         ],
+		methods: {
+			personage_1: function(){
+				this.parent.htmlLink.style.display = "none";
+				modules.personage =  addTileCommon(personageId, this.$props().sprites[typeP1], [250, 300]);
+				pesonageType = typeP1;
+				createSocket();
+            },
+			personage_2: function(){
+				this.parent.htmlLink.style.display = "none";
+				modules.personage =  addTileCommon(personageId, this.$props().sprites[typeP2], [250, 300]);
+				pesonageType = typeP2;
+                createSocket();				
+            },
+			
+			///добавляем код нового персонажа  - personage_3 (изменяем только typeP3)
+			personage_3: function(){ 
+				this.parent.htmlLink.style.display = "none";
+				modules.personage =  addTileCommon(personageId, this.$props().sprites[typeP3], [250, 300]);
+				pesonageType = typeP3;
+                createSocket();				
+            }		
+		}			
+		
+ }
+
+///При добавлении нового персонажа также добавить в файл index.html код с именем персонажа - personage_3, по анологии с остальным текстом
+
+
+							<div data-chose_personage="container" class="form-group " style="display: none; padding: 10px; background-color: white; position: fixed;  top: 5px; left: 5px; z-index:5">
+								<p>Выберите персонажа:</p>
+								<div class="form-check">
+									<input  class="form-check-input" type="radio" name="personage_1" id="flexRadioDefault1">
+									<label class="form-check-label" for="flexRadioDefault1">
+										personage_1
+									</label>
+								</div>
+								<div class="form-check">
+									<input class="form-check-input" type="radio" name="personage_2" id="flexRadioDefault2">
+									<label class="form-check-label" for="flexRadioDefault2">
+										personage_2
+									</label>
+							     </div>	
+								 
+								 <div class="form-check">
+									<input class="form-check-input" type="radio" name="personage_3" id="flexRadioDefault2">
+									<label class="form-check-label" for="flexRadioDefault2">
+										new personage
+									</label>
+							     </div>	
+							</div>
+
+
+
+Для того чтобы выложить игру на glitch, создать новый проект "glitch-hello-node"
+Удалить все файлы кроме .env .gitignore, лишние папки(src), с папки public удалить все файлы
+Скопировать все файлы в проект и папку publick 
+Нажать кнопку prewiew -> prewiew in new window
+
---
+++ collagen_2/games/town/public/tiles.js
@@ -0,0 +1,221 @@
+
+
+
+
+function Tile(id, sprite, point){
+	
+    this.type = "tile";		
+	this.id = id;
+	this.parent = sprite.id;
+	this.frame = sprite.frame;
+	this.frame_collection = sprite.frame_collection;///варианты изображения спрайта
+	this.frame_index = sprite.frame_index;
+	this.point = point.slice(0); //верхяя левая точка спрайта на канвас
+	this.point2 = sprite.point2.slice(0); //нижняя правая
+
+
+	this.rotate = sprite.rotate; //вращение относительно начального при создании спрайта
+	this.show = true;
+
+	this.scale_x = sprite.scale_x; //масштаб относительно изначального при создании спрайта
+	this.scale_y = sprite.scale_x;
+	this.width = sprite.width;
+	this.height = sprite.height;
+}
+
+///более быстрый рендер спрайта, не рисует контур, границу и текст
+Tile.prototype.render_ = function(){
+	if(!this.show)return;		
+	if(this.rotate !== 0){ //вращение спрайта
+		var halfW = this.width/2;
+		var halfH = this.height/2;		
+	    //var area = rotationArea(this.area_1, this.rotate);		
+			var move = [this.point[0]+  halfW, this.point[1] + halfH]; //translate в точку move для вращения canvas
+			ctx.save();
+			ctx.translate(move[0],   move[1]);				
+			ctx.rotate(this.rotate); 		
+			ctx.drawImage(this.frame, -halfW, -halfH, this.width, this.height);		
+			ctx.restore();			
+	}else{//без вращения		
+		ctx.drawImage(this.frame, this.point[0], this.point[1], this.width, this.height);
+	}
+}
+
+ function renderAllTiles(){
+	 	      for(var i = 0; i< tiles_bg.length;  i++){
+				tiles_bg[i].render_();
+			  }
+			  for(var i = 0; i< tiles_common.length;  i++){
+				tiles_common[i].render_();
+			  }	 
+ }
+
+
+//цикл для анимации спрайтов 
+function animationLopLayer(spritesArrBg, spritesArr, speed, sprites){                
+  //spritesArrBg.sort(compare);
+ 
+  var off = {isOff: false};
+  let prev = performance.now();
+  function lop() {
+
+	 var lt = performance.now();
+     var time = lt - prev; 
+     if (time > speed ){
+	  	  //очистка канвас 
+  	  ctx.translate(0, 0);
+	  //ctx.clearRect(0, 0, srcWidth , srcHeight);
+	  ctx.fillRect(0, 0, srcWidth , srcHeight);
+	        spritesArr.sort(compare);
+	        ctx.save();///////////////
+            ctx.translate(ctxTranslate[0], ctxTranslate[1]);	
+    ///
+		    prev = lt;			
+	        // ctx.putImageData(saveImg, 0, 0);
+			  //ctx.clearRect(0, 0, srcWidth , srcHeight);
+			  for(var i = 0; i< spritesArrBg.length;  i++){
+				spritesArrBg[i].render_();
+			  }
+			  for(var i = 0; i< spritesArr.length;  i++){
+				spritesArr[i].render_();
+			  }
+	} 
+	ctx.restore();
+	if(!off.isOff)requestAnimationFrame(lop);
+  }
+  lop();
+ return  off;
+ 
+	function compare(a, b) {
+		if (a.point[1]+a.height > b.point[1]+b.height ) return 1; // если первое значение больше второго
+		if (a.point[1]+a.height  == b.point[1]+b.height ) return 0; // если равны
+		if (a.point[1]+a.height  < b.point[1]+b.height ) return -1; // если первое значение меньше второго
+	}
+}
+
+//включает следующюю картинку из колекции 
+Tile.prototype.nextFrame = function(index){
+	this.frame_index +=1;
+	if(index || index ===  0)this.frame_index = index;
+	if(this.frame_index > this.frame_collection.length -1 ){
+		this.frame_index = 0;
+	}
+	this.frame = this.frame_collection[this.frame_index];	
+}
+Tile.prototype.prevFrame = function(index){	
+	this.frame_index -=1;
+	if(index || index ===  0)this.frame_index = index;
+	if(this.frame_index < 0  ){
+		this.frame_index = this.frame_collection.length -1;
+	}
+	this.frame = this.frame_collection[this.frame_index];	
+}
+
+///сдвигает спрайт 
+Tile.prototype.move = function(x, y){					
+						this.point[0]+=x;
+						this.point[1]+=y;
+}
+Tile.prototype.moveConer = function(x, y, bottom){	//перемещает спрайт в указанные координаты	                      						
+                        this.point[0] = x;
+						if(bottom){this.y = y-this.height; return;}
+						this.point[1] = y;						
+}
+
+
+Tile.prototype.moveCenter= function(x,y){
+		                this.point[0] = x-this.width/2;
+						 this.point[1] = y-this.height/2;
+}
+
+Tile.prototype.rotateFi = function(fi){
+	this.rotate =  parseInt(fi)* Math.PI / 180;
+}
+//масштабирование 
+Tile.prototype.scale = function(coeff_x, coeff_y){
+	if(coeff_x == this.scale_x && coeff_y == this.scale_y)return;
+	
+	var current_scale_x = coeff_x/this.scale_x; var current_scale_y = coeff_y/this.scale_y;
+	var area = scaleArea([[this.point[0], this.point[1]], [this.point[0]+this.width, this.point[1]], 
+	    [this.point[0]+this.width, this.point[1]+this.height], [this.point[0], this.point[1]+this.height]], current_scale_x, current_scale_y, false);
+    
+	var imgBox = getBox(area);
+	this.point = imgBox[0];
+	this.point2 = imgBox[1];
+	////////////
+	this.width = this.point2[0] - this.point[0];
+	this.height = this.point2[1] - this.point[1];
+	/////////////
+	this.scale_x = coeff_x;
+	this.scale_y = coeff_y;	
+}
+
+////обновляет порядок в массиве tiles_common согласно массиву tiles_common_save
+function updateCommonTiles(sprites){
+	tiles_common = [];
+	for(var i=0; i<tiles_common_save.length; i++){
+		if(sprites[tiles_common_save[i].parent])tiles_common.push(new Tile(tiles_common_save[i].id, sprites[tiles_common_save[i].parent], tiles_common_save[i].point));
+	}
+}
+function updateBgTiles(sprites){ 
+	tiles_bg = [];
+	for(var i=0; i<tiles_bg_save.length; i++){
+		if(sprites[tiles_bg_save[i].parent])tiles_bg.push(new Tile(tiles_bg_save[i].id, sprites[tiles_bg_save[i].parent], tiles_bg_save[i].point));
+	}
+}
+
+function updateCollisionTiles(){ 
+	tiles_collision = [];
+	for(var i=0; i<tiles_collision_save.length; i++){
+		tiles_collision.push(tiles_collision_save[i]);
+	}
+}
+
+///определение столкновений
+//obgectsArr, tile, dx, dy массив непрходимых участков, объект персонажа
+///dx, dy - процент от в долях от ширины и высоты спрайта 
+//для определения области столкновения относительно спрайта
+// для определения столкновений по всей высоте и ширине спрайта указать dx =1; dy =1;
+                                              ///0.4 0.2
+function collisionDetection(obgectsArr, tile, dx, dy){
+	var collision = false;
+	
+				///определяем размер области ног персонажа для определения столкновений
+				var p1 = tile.point.slice(0);
+				p1[1] = p1[1]+ tile.height*(1-dy);
+				var t_h = tile.height*dy;	
+				p1[0] = p1[0]+ tile.width*(1 - dx)/2;
+				var t_w = tile.width*dx;
+	
+	for(var i=0; i< obgectsArr.length; i++){
+		
+		var ds = getDistance([obgectsArr[i].point[0]+obgectsArr[i].width/2, obgectsArr[i].point[1]+obgectsArr[i].height/2], p1);
+		///если расстояние до непроходимой области меньше ширины и высоты спрайта + ширины/2 и высоты/2 обасти
+		///возможно столкновение
+		if(Math.abs(ds[0]) < obgectsArr[i].width/2 + t_w && Math.abs(ds[1]) < obgectsArr[i].height/2 + t_h){
+			    // console.log("столкновение возможно");
+				
+				///для коррректной работы метода, площадь непроходимого участка
+				// должна быть по высоте и ширине больше части площади персонажа для определения столкновения 
+				
+				var p2 = obgectsArr[i].point.slice(0);
+				var o_w = obgectsArr[i].width; var o_h = obgectsArr[i].height;
+				
+				
+				///Определям попадение одной из точек области персонажа в непроходимую область
+				if(p1[0] > p2[0] && p1[0] < p2[0] + o_w && p1[1] > p2[1] && p1[1] < p2[1] + o_h){								
+						collision = [p1[0], p1[1]];		
+				}else if(p1[0] > p2[0] && p1[0] < p2[0] + o_w && p1[1]+t_h > p2[1] && p1[1]+t_h < p2[1] + o_h){ 
+						collision = [p1[0], p1[1]+t_h];		
+				}else if(p1[0]+t_w > p2[0] && p1[0]+t_w < p2[0] + o_w && p1[1]+t_h > p2[1] && p1[1]+t_h < p2[1] + o_h){ 
+						collision = [p1[0]+t_w, p1[1]+t_h];		
+				}else if(p1[0]+t_w > p2[0] && p1[0]+t_w < p2[0] + o_w && p1[1] > p2[1] && p1[1] < p2[1] + o_h){ 
+						collision = [p1[0]+t_w, p1[1]];		
+				}		
+		}		
+	}
+	return collision;
+}
+
+
+///ctx.isPointInPath(cutPathArea, tmpX, tmpY)
